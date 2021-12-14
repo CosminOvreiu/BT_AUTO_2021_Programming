0e6878ee87d91b8b7a61ac62ad425650a0e9fc8b
cosmin ovreiu <ovreiu.cosmin@yahoo.ro>
2021-12-14 10:07:18 +0200

    Class vs Object course

--- BT_AUTO_2021_Programming/Program.cs
+++ BT_AUTO_2021_Programming/Program.cs
@@ -6,6 +6,12 @@ namespace BT_AUTO_2021_Programming
     {
         static void Main(string[] args)
         {
+            //Course01(args);
+            Course02(args);
+        }
+
+        static void Course01(string[] args)
+        {
             const int MAX_SIZE = 100;
             const double PI = 3.141592;
             const int MAX_NUMBER = 5;
@@ -209,32 +215,88 @@ namespace BT_AUTO_2021_Programming
             }
 
 
-            int year = 1900;
-            int febDays = 28;
 
-            if (year % 4 == 0)
-            {
-                if (year % 100 == 0 && year % 400 ! = 0)
-                {
-                    febDays = 29;
-                }
-                if (year % 100 == 0 && year % 400 == 0)
-                {
-                    febDays = 28;
-                }
-                if (year % 100 != 0)
-                {
-                    febDays = 29;
-                }
-            }
-            else
-            {
-                febDays = 28;
-            }
-            Console.WriteLine("February has {0} days", febDays);
 
+            foreach (string yr in args)
+            {
+
+                int febDays = 28;
+                int year = int.Parse(yr);
+
+
+                /*if (year % 4 == 0)
+                {
+                    if (year % 100 == 0 && year % 400 ! = 0)
+                    {
+                        febDays = 29;
+                    }
+                    if (year % 100 == 0 && year % 400 == 0)
+                    {
+                        febDays = 28;
+                    }
+                    if (year % 100 != 0)
+                    {
+                        febDays = 29;
+                    }
+                }*/
+                if (year < 1900 || year > 2016)
+                {
+                    Console.WriteLine("Year is out of valid bounds (1900-2016)");
+                }
+                else
+                {
+                    if ((year % 4 == 0 && year % 100 == 0 && year % 400 != 0) || (year % 4 == 0 && year % 100 != 0))
+                    {
+                        febDays = 28;
+                    }
+
+                    Console.WriteLine("February has {0} days", febDays);
+                }
+
+            }
 
 
         }
+
+        static void Course02(string[] args)
+        {
+            Circle c1 = new Circle();
+            Circle c2; // c2 will be null !!!
+            c1.SetRadius(10);
+            /* double area = c1.GetArea();*/
+            /* Console.WriteLine(c1.GetArea());*/
+            c1.PrintCircle();
+            Circle c3 = new Circle();
+            c3.SetRadius(5);
+            /*Console.WriteLine(c3.GetArea());*/
+            c3.PrintCircle();
+            foreach (string p in args)
+            {
+                Circle c = new Circle();
+                c.SetRadius(Double.Parse(p));
+                Square s = new Square();
+                s.SetSide(Double.Parse(p));
+                Rectangle r = new Rectangle();
+                r.SetSize(Double.Parse(p), Double.Parse(p));
+                c.PrintCircle();
+                s.PrintSquare();
+                r.PrintRectangle();
+
+            }
+
+            Person p1 = new Person();
+            p1.SetName("Alex");
+            p1.SetSex('m');
+            p1.Eat();
+            p1.Run();
+            p1.Eat();
+            p1.PrintPerson();
+
+            Rectangle r1 = new Rectangle();
+            r1.SetSize(2, 3);
+            r1.PrintRectangle(); 
+
+        }
+
     }
 }
