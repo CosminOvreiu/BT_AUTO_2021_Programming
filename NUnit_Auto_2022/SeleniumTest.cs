523d7e3e4761737da26e246d6197990bf79f131c
cosmin ovreiu <ovreiu.cosmin@yahoo.ro>
2022-01-16 18:55:37 +0200

    Selenium, Chrome, Firefox,Edge

---
+++ NUnit_Auto_2022/SeleniumTest.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnit_Auto_2022
+{
+    class SeleniumTest
+    {
+
+        
+        IWebDriver driver;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            //driver = new EdgeDriver();
+        }
+
+        [Test]
+        public void Test01()
+        {
+           
+            driver.Url = "https://google.com";
+            driver.Navigate();
+
+        }
+
+        [Test]
+        public void Test02()
+        {
+
+            driver.Url = "https://learn.digitalstack.ro";
+            driver.Navigate();
+
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Quit();
+        }
+
+
+
+    }
+}
