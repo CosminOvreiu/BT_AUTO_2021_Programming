c7a08be7f7ff51f61691402416e78c5cba35309d
cosmin ovreiu <ovreiu.cosmin@yahoo.ro>
2022-01-18 21:15:04 +0200

    Webelements locators / selectors

--- NUnit_Auto_2022/SeleniumTest.cs
+++ NUnit_Auto_2022/SeleniumTest.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,24 +12,37 @@ namespace NUnit_Auto_2022
     class SeleniumTest
     {
 
-        
         IWebDriver driver;
+        const string protocol = "https";
+        const string hostname = "magazinulcolectionarului.ro";
+        const string path = "/";
+
+        string url = protocol + "://" + hostname + path;
+
 
 
         [SetUp]
         public void Setup()
         {
             driver = new ChromeDriver();
+            //driver = new FirefoxDriver();
             //driver = new EdgeDriver();
         }
 
         [Test]
         public void Test01()
         {
-           
-            driver.Url = "https://google.com";
+
+            driver.Url = url;
             driver.Navigate();
 
+            //driver.Navigate().GoToUrl(url);
+
+            driver.Navigate().Back();
+
+            driver.Navigate().Forward();
+
+            driver.Navigate().Refresh();
         }
 
         [Test]
@@ -39,6 +53,32 @@ namespace NUnit_Auto_2022
             driver.Navigate();
 
         }
+
+
+        [Test]
+        /*public void test03()
+           
+        {
+            driver.Url = url;
+            driver.Navigate();
+
+
+            IWebElement body = driver.FindElement(By.ClassName("modal-header"));
+            body.Click();
+
+            IWebElement eaglemoss = driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div/div[2]/nav/div/ul/li[1]/a/span[2]"));
+            eaglemoss.Click();
+
+            var addToCart = driver.FindElements(By.XPath("//*[@id='amasty-shopby-product-list']/div[2]/ol/li"));
+            foreach(IWebElement el in addToCart)
+            {
+                var text = el.FindElement(By.XPath("//span"));
+                Console.WriteLine(text.Text);
+            }
+        
+
+        }
+            */
 
         [TearDown]
         public void Teardown()
